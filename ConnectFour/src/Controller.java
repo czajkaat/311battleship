bbb80a3af5c3413c809ff3b0fbc990e6fe22b855
santoscw <santoscw@miamioh.edu>
2020-03-01 14:44:05 -0500

    Added basic outline of controller.java

--- ConnectFour/src/Controller.java
+++ ConnectFour/src/Controller.java
@@ -1,4 +1,50 @@
 
 public class Controller {
 
+	public static void main(String args[]) {
+		// Determine which view to use
+		int viewChoice = 0;
+		if (args[0].equals("1")) {
+			System.out.println("Text View");
+			viewChoice = 1;
+		} else if (args[0].equals("2")) {
+			System.out.println("GUI View");
+			viewChoice = 2;
+		}
+		
+		// Call on View to start up game with viewChoice
+		View display = new View();
+		
+		// Initiate variables in Model class
+		Model game = new Model();
+		char[][] gameBoard = game.getBoard();
+		int turn = 1;
+		// Game Loops until winner is determined
+		while(!game.hasWinner())) {
+			display.board = 
+			
+			// Display the current board
+			if (viewChoice == 1) {
+				display.drawTextBoard();
+			}
+			
+			// Display which player's turn
+			if (turn % 2 == 0) {
+				// Player 2's turn
+				
+			} else {
+				//Player 1's turn
+				
+			}
+			
+			// Get user input
+			
+			
+			// Update game board
+			
+		}
+		
+		// Display winner
+		
+	}
 }
