467e3ec592da613470a5dc17fa94af8bc6b7203d
mortons2 <49290141+mortons2@users.noreply.github.com>
2020-03-01 14:47:05 -0500

    Model Template

--- ConnectFour/src/Model.java
+++ ConnectFour/src/Model.java
@@ -1,4 +1,62 @@
+import java.util.Arrays;
 
 public class Model {
+	char[][] board;
+	int turn; 
+	
+	
+	public Model() {
+		board = new char[7][6];
+		turn = 0; 
+	}
+	
+	public void clearBoard() {
+		for(char[] row:board)
+			Arrays.fill(row, 'e');
+	}
+	
+	public void newGame() {
+		clearBoard();
+		turn = 0;
+	}
+	
+	public char turn() {
+		char player;
+		if(turn % 2 == 0)
+			player = 'x';
+		else
+			player = 'o';
+		
+		return player;
+	}
+	
+	public char gameState() {
+		
+		return 'e';
+	}
+	
+	public boolean playPiece(int column) {
+		if(column<0 || column >6)
+			return false;
+		for(int row = 0;row < 6;row++) {
+			if(board[column][row]=='e') {
+				playHere(column, row);
+				turn++;
+				return true;
+			}
+		}
+		
+		return false; 
+	}
+	
+	private void playHere(int column, int row) {
+		char player = turn();
+		
+		board[column][row] = player;
+	}
+	
+	public char[][] returnBoard() {
+		return board.clone();
+	}
 
 }
