406519453efc669b89b520ebc3cd6ee9a080ebcd
mortons2 <49290141+mortons2@users.noreply.github.com>
2020-03-01 15:36:15 -0500

    Comments

--- ConnectFour/src/Model.java
+++ ConnectFour/src/Model.java
@@ -1,5 +1,9 @@
 import java.util.Arrays;
 
+/**
+ * @author smort
+ *
+ */
 public class Model {
 	final int width = 7;
 	final int height = 6;
@@ -7,21 +11,37 @@ public class Model {
 	int turn; 
 	
 	
+	/**Constructor
+	 * Initializes board, fills it with 'e's and sets turn to 0
+	 * 
+	 */
 	public Model() {
 		board = new char[width][height];
+		clearBoard();
 		turn = 0; 
 	}
 	
+	/**
+	 * Sets all board positions to 'e' 
+	 */
 	public void clearBoard() {
 		for(char[] row:board)
 			Arrays.fill(row, 'e');
 	}
 	
+	/** Sets the board to empty and resets turn counter.
+	 * 
+	 */
 	public void newGame() {
 		clearBoard();
 		turn = 0;
 	}
 	
+	/**Gives the current player turn
+	 * @return the char of the current player
+	 * 'x' = player 1 (goes first)
+	 * 'o' = player 2
+	 */
 	public char turn() {
 		char player;
 		if(turn % 2 == 0)
@@ -32,6 +52,10 @@ public class Model {
 		return player;
 	}
 	
+	/**gameState gives the current state or winner of the current board
+	 * @return current game state. 'e' if in progress, 'c' for a game with no winners
+	 * 			'x' for a player 1 win, 'o' for a player 2 win
+	 */
 	public char gameState() {
 		char player = turn();
 		//Row Win
@@ -86,6 +110,10 @@ public class Model {
 		return 'e';
 	}
 	
+	/**Plays the next piece at the given position
+	 * @param column is where the next piece will be played
+	 * @return true if the move is possible, false if the given column is full
+	 */
 	public boolean playPiece(int column) {
 		if(column<0 || column >6)
 			return false;
@@ -100,12 +128,19 @@ public class Model {
 		return false; 
 	}
 	
+	/**Plays a piece at a specific x,y position. 
+	 * @param column
+	 * @param row
+	 */
 	private void playHere(int column, int row) {
 		char player = turn();
 		
 		board[column][row] = player;
 	}
 	
+	/**Returns a clone of the current game board
+	 * @return char[][] copy of the current board (width,height)
+	 */
 	public char[][] returnBoard() {
 		return board.clone();
 	}
